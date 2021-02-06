24c9a595472b3053e5ac8feb88ce3c4817e9c729
Lennart Eichhorn <lennart@madmanfred.com>
2021-02-06 17:27:48 +0100

    qtcreator: 4.11.0 -> 4.14.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11388,7 +11388,7 @@ in
 
   pyrseas = callPackage ../development/tools/database/pyrseas { };
 
-  qtcreator = libsForQt514.callPackage ../development/tools/qtcreator { };
+  qtcreator = libsForQt5.callPackage ../development/tools/qtcreator { };
 
   qxmledit = libsForQt5.callPackage ../applications/editors/qxmledit {} ;
 

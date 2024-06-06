85a8d987886355e277fd6d52d36abf24112fe048
Zebreus <lennart@zebre.us>
2024-06-06 17:42:23 +0200

    test-a

--- pkgs/by-name/pi/pixelpwnr-server/package.nix
+++ pkgs/by-name/pi/pixelpwnr-server/package.nix
@@ -1,5 +1,21 @@
-{ lib, stdenv, rustPlatform, fetchFromGitHub, cmake, freetype, libX11
-, libXcursor, libXrandr, libXi, libGL, pkg-config }:
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  fetchFromGitHub,
+  cmake,
+  freetype,
+  libX11,
+  libXcursor,
+  libXrandr,
+  libXi,
+  libGL,
+  pkg-config,
+  autoPatchelfHook,
+  libgcc,
+  wrapGAppsHook4,
+  makeWrapper,
+}:
 
 rustPlatform.buildRustPackage {
   pname = "pixelpwnr-server";
@@ -14,22 +30,72 @@ rustPlatform.buildRustPackage {
 
   cargoHash = "sha256-N3NXXIX5kzEmKTDpNzJgnSqJWNctupjm69mGYVteKIU=";
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    # autoPatchelfHook
+    makeWrapper
+    # wrapGAppsHook4
+  ];
 
-  buildInputs = [ freetype libX11 libXcursor libXrandr libXi ];
+  buildInputs = [
+    freetype
+    libX11
+    libXcursor
+    libXrandr
+    libXi
+    # libGL
+    # libgcc
+  ];
+
+  runtimeDependencies = [ ];
 
   postFixup = lib.optionalString stdenv.targetPlatform.isElf ''
-    patchelf $out/bin/pixelpwnr-server --add-needed libGL.so --add-rpath ${
-      lib.makeLibraryPath [ libGL ]
-    }
+    # ldd $out/bin/pixelpwnr-server
+    # patchelf $out/bin/pixelpwnr-server --add-rpath $ {
+    #   lib.makeLibraryPath [
+    #     libGL
+    #     libX11
+    #     libXcursor
+    #     libXrandr
+    #     libXi
+    #   ]
+    # }
+    # ldd $out/bin/pixelpwnr-server
   '';
+
+  postInstall = ''
+    ldd $out/bin/pixelpwnr-server
+    # patchelf $out/bin/pixelpwnr-server \
+    #   --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+    #   $out/bin/pixelpwnr-server
+    # patchelf $out/bin/pixelpwnr-server --add-needed libGL.so --add-rpath \
+    #   ${lib.makeLibraryPath [ libGL ]}
+    wrapProgram $out/bin/pixelpwnr-server \
+        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL ]}
+  '';
+
+  doCheck = false;
+  # buildInputs = [
+  #   freetype
+  #   libX11
+  #   libXcursor
+  #   libXrandr
+  #   libXi
+  #   libGL
+  #   libgcc
+  # ];
+
+  # postInstall = ''
+  #   ldd $out/bin/pixelpwnr-server
+  #   wrapProgram $out/bin/pixelpwnr-server \
+  #       --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL ]}
+  # '';
 
   meta = with lib; {
     homepage = "https://timvisee.com/projects/pixelpwnr-server";
-    description =
-      "Blazingly fast GPU accelerated pixelflut server written in Rust";
+    description = "Blazingly fast GPU accelerated pixelflut server written in Rust";
     license = licenses.gpl3;
     mainProgram = "pixelpwnr-server";
   };
 }
-

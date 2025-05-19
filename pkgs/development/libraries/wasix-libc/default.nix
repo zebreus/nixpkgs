d92398b7fe1531f82bb4bfd39391ab79736bdcc6
Zebreus <zebreus@zebre.us>
2025-05-19 11:38:02 +0200

    Add wasix-libc

---
+++ pkgs/development/libraries/wasix-libc/default.nix
@@ -0,0 +1,88 @@
+{
+  stdenv,
+  buildPackages,
+  fetchFromGitHub,
+  lib,
+  firefox-unwrapped,
+  firefox-esr-unwrapped,
+}:
+
+let
+  pname = "wasix-libc";
+  # TODO: Update to the correct version
+  version = "unstable-2025";
+in
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = buildPackages.fetchFromGitHub {
+    owner = "wasix-org";
+    repo = "wasix-libc";
+    rev = "a5499ecac5d07e874fa5cbd8cd2c4baddc3d8942";
+    hash = "sha256-FseTYcWNMg3p1ra8a/Vx7U8e/Y00kh86OmXRTw0vqy0=";
+    fetchSubmodules = true;
+  };
+
+  outputs = [
+    "out"
+    "dev"
+    "share"
+  ];
+
+  # clang-13: error: argument unused during compilation: '-rtlib=compiler-rt' [-Werror,-Wunused-command-line-argument]
+  postPatch = ''
+    substituteInPlace Makefile \
+      --replace "-Werror" ""
+
+    # Remove the test for exports.
+    # TODO: Figure out why the output is different than what is expected.
+    sed -i "771d" Makefile
+  '';
+
+  preBuild = ''
+    export SYSROOT_LIB=${builtins.placeholder "out"}/lib
+    export SYSROOT_INC=${builtins.placeholder "dev"}/include
+    export SYSROOT_SHARE=${builtins.placeholder "share"}/share
+    mkdir -p "$SYSROOT_LIB" "$SYSROOT_INC" "$SYSROOT_SHARE"
+    makeFlagsArray+=(
+      "SYSROOT_LIB:=$SYSROOT_LIB"
+      "SYSROOT_INC:=$SYSROOT_INC"
+      "SYSROOT_SHARE:=$SYSROOT_SHARE"
+      # # https://bugzilla.mozilla.org/show_bug.cgi?id=1773200
+      # "BULK_MEMORY_SOURCES:="
+      PIC:=yes
+    )
+
+    export TARGET_ARCH=wasm32
+    export TARGET_OS=wasix
+  '';
+
+  enableParallelBuilding = true;
+
+  # We just build right into the install paths, per the `preBuild`.
+  dontInstall = true;
+
+  preFixup = ''
+    ln -s $share/share/undefined-symbols.txt $out/lib/wasi.imports
+  '';
+
+  passthru.tests = {
+    inherit firefox-unwrapped firefox-esr-unwrapped;
+  };
+
+  meta = with lib; {
+    changelog = "https://github.com/WebAssembly/wasi-sdk/releases/tag/wasi-sdk-${version}";
+    description = "WASI libc implementation for WebAssembly";
+    homepage = "https://wasi.dev";
+    platforms = platforms.wasix;
+    maintainers = with maintainers; [
+      matthewbauer
+      rvolosatovs
+    ];
+    license = with licenses; [
+      asl20
+      llvm-exception
+      mit
+    ];
+  };
+}
